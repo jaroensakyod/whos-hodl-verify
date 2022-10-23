d88fd8b0d82fc74530f6a34b906a0cc22e32a012
jaroensakyod <jaroensak.yod@gmail.com>
2022-10-23 23:27:03 +0700

    add connect wallet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import { Web3Modal } from '@web3modal/react'
+import {chains, providers} from "@web3modal/ethereum"
+
+
+
+const modalConfig = {
+          theme: "dark",
+          accentColor:"default",
+          ethereum:{
+                    appName: "Web3Modal",
+                    chains: [chains.goerli, chains.polygonMumbai],
+                    providers: [
+                              providers.walletConnectProvider({
+                                        projectId: "b81bdb9ce7441f4bfdd041942a612a40"
+                              }),
+                    ],
+                    autoConnect:true,
+          },
+          projectId: "b81bdb9ce7441f4bfdd041942a612a40",
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+          <>
+                    <Component {...pageProps} />
+                    <Web3Modal config={modalConfig}/>
+          </>
+  );
 }
 
 export default MyApp
